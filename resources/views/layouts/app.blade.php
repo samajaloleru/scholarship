945aa902572ce9037a306e6e94a011b481905a8f
samajaloleru <samsonajaloleru@gmail.com>
2021-07-10 12:11:19 +0100

    Quick Push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/main.min.css">
-        <link rel="stylesheet" href="css/button.min.css">
+        <link href="css/main.min.css" rel="stylesheet">
+        <link href="css/button.min.css" rel="stylesheet">
         <title>{{config('app.name', 'LSAPP')}}</title>
 
         <!-- Scripts -->
